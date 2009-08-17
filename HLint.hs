7ac44a6d5d12d68026dfb985432b6c3ae91521d8
Neil Mitchell <ndmitchell@gmail.com>
2009-08-17 17:23:44 +0000

    Add test sutie

--- HLint.hs
+++ HLint.hs
@@ -1,6 +1,12 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables #-}
 
 import System.Console.CmdArgs
+
+-- for the tests only
+import Control.Monad
+import System.Environment
+import Control.Exception
+
 
 data HLint = HLint
     {report :: [FilePath]
@@ -13,7 +19,7 @@ data HLint = HLint
     ,cpp_include :: [String]
     ,files :: [String]
     }
-    deriving (Data,Typeable,Show)
+    deriving (Data,Typeable,Show,Eq)
 
 hlint = mode $ HLint
     {report = def & empty "report.html" & typFile & text "Generate a report in HTML"
@@ -30,4 +36,30 @@ hlint = mode $ HLint
                ,"To check all Haskell files in 'src' and generate a report type:","  hlint src --report"]
 
 
-main = print =<< cmdArgs "HLint v1.6.5, (C) Neil Mitchell 2006-2009" hlint
+main = print =<< cmdMode "HLint v1.6.5, (C) Neil Mitchell 2006-2009" hlint
+
+
+smoke = do
+    let v = modeValue hlint
+    [] === v
+    fails ["-ch"]
+    ["--colo"] === v{color=True}
+    ["-ct"] === v{color=True,test=True}
+    ["-thfoo"] === v{test=True,hint=["foo"]}
+    ["-cr"] === v{color=True,report=["report.html"]}
+    ["--cpp-define=val","x"] === v{cpp_define=["val"],files=["x"]}
+    fails ["--cpp-define"]
+    ["--cpp-define","val","x","y"] === v{cpp_define=["val"],files=["x","y"]}
+    putStrLn "Tests successful"
+    where
+
+(===) args v = do
+    res <- withArgs args $ cmdMode "" hlint
+    when (res /= v) $
+        error $ "Mismatch on flags " ++ show args
+
+fails args = do
+    res <- try $ withArgs args $ cmdMode "" hlint
+    case res of
+        Left (e :: SomeException) -> return ()
+        Right _ -> error $ "Expected failure " ++ show args
