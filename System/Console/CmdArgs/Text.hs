87026e10f34b711e3cf1078fb3f3f1e27b4d72f8
Neil Mitchell <ndmitchell@gmail.com>
2010-06-16 10:06:36 +0000

    Rework the Text representation

--- System/Console/CmdArgs/Text.hs
+++ System/Console/CmdArgs/Text.hs
@@ -2,10 +2,11 @@
 
 module System.Console.CmdArgs.Text(TextFormat(..), Text(..), showText) where
 
+import Control.Arrow
 import Data.Char
-import Data.Maybe
+import Data.Function
 import Data.List
-import Text.Format.Para
+import Data.Maybe
 
 
 defaultTextWidth = 70
@@ -28,7 +29,7 @@ instance Read TextFormat where
 
 
 data Text = Line String -- a single line
-          | Cols (String,String,String) -- a single line with three columns (always indented by 2 spaces)
+          | Cols [String] -- a single line with columns (always indented by 2 spaces)
 
 instance Show Text where
     showList = showString . showText defaultTextWidth
@@ -41,19 +42,24 @@ showHelp Simple = showSimple
 showHelp (Text x) = showText (fromMaybe defaultTextWidth x)
 
 
+---------------------------------------------------------------------
+-- TEXT OUTPUT
+
 showText :: Int -> [Text] -> String
 showText width xs = unlines $ concatMap f xs
     where
-        (as,bs,cs) = unzip3 [x | Cols x <- xs]
-        an = maximum (map length as) + 1
-        bn = maximum (map length bs) + 1
-        abn = 2 + an + bn
+        cs :: [(Int,[Int])]
+        cs = map (\x -> (fst $ head x, map maximum $ transpose $ map snd x)) $
+                groupBy ((==) `on` fst) $ sortBy (compare `on` fst)
+                [(length x, map length $ init x) | Cols x <- xs]
         pad n x = x ++ replicate (n - length x) ' '
 
         f (Line x) = map (a++) $ wrap (width - length a) x
             where (a,b) = span isSpace x
-        f (Cols (a,b,c)) = ("  " ++ pad an a ++ pad bn b ++ c1) : map (replicate abn ' ' ++) cs 
-            where c1:cs = if null c then [""] else wrap (width - abn) c
+
+        f (Cols xs) = (concatMap ("  "++) $ zipWith pad ys xs ++ [z1]) : zs
+            where ys = fromJust $ lookup (length xs) cs
+                  z1:zs = if null (last xs) then [""] else wrap (width - sum ys - (2 * length xs)) (last xs)
 
 
 -- | Split the text into strips of no-more than the given width
@@ -73,24 +79,35 @@ wrap width = combine . split
         combine [] = []
 
 
+---------------------------------------------------------------------
+-- HTML OUTPUT
+
 showHTML :: [Text] -> String
 showHTML xs = unlines $
     ["<table class='cmdargs'>"] ++
     map f xs ++
     ["</table>"]
     where
-        f (Line x) = "<tr><td colspan='3'" ++ (if null a then "" else " class='indent'") ++ ">" ++ escape b ++ "</td></tr>"
-            where (a,b) = span isSpace x
-        f (Cols (a,b,c)) = "<tr><td class='indent'>" ++ intercalate "</td><td>" (map escape [a,b,c]) ++ "</td></tr>"
+        cols = maximum [length x | Cols x <- xs]
 
-        escape :: String -> String
-        escape "" = "&nbsp;"
-        escape xs = concatMap g xs
+        f (Line x) = tr $ td cols (null a) b
+            where (a,b) = span isSpace x
+        f (Cols xs) = tr $ concatMap (td 1 True) (init xs) ++ td (cols + 1 - length xs) True (last xs)
+
+        tr x = "<tr>" ++ x ++ "</tr>"
+        td cols indent x = "<td" ++ (if cols == 1 then "" else " colspan='" ++ show cols ++ "'")
+                                 ++ (if indent then " class='indent'" else "") ++ ">" ++
+                           text x ++ "</td>"
+        text "" = "&nbsp;"
+        text xs = concatMap g xs
             where g '&' = "&amp;"
                   g '>' = "&gt;"
                   g '<' = "&lt;"
                   g x = [x]
 
+
+---------------------------------------------------------------------
+-- SIMPLE OUTPUT
 
 showSimple :: [Text] -> String
 showSimple xs = "I have no idea what simple format is, but I'm guess it should go direct from Mode..."
