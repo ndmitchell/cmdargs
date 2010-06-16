4016a50372eaa1bf9e9503ea094bc9e321f72a8d
Neil Mitchell <ndmitchell@gmail.com>
2010-06-16 06:17:11 +0000

    Move the Text module up one level

--- System/Console/CmdArgs/Explicit/Help.hs
+++ System/Console/CmdArgs/Explicit/Help.hs
@@ -2,7 +2,7 @@
 module System.Console.CmdArgs.Explicit.Help where
 
 import System.Console.CmdArgs.Explicit.Type
-import System.Console.CmdArgs.Explicit.Text
+import System.Console.CmdArgs.Text
 
 instance Show (Mode a) where
     show = show . helpText
