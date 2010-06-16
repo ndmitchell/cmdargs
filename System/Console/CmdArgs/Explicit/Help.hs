21f04553b3037c97b01692d80dfbf21df7712303
Neil Mitchell <ndmitchell@gmail.com>
2010-06-16 10:09:07 +0000

    Implement the Help feature for Explicit

--- System/Console/CmdArgs/Explicit/Help.hs
+++ System/Console/CmdArgs/Explicit/Help.hs
@@ -3,10 +3,39 @@ module System.Console.CmdArgs.Explicit.Help where
 
 import System.Console.CmdArgs.Explicit.Type
 import System.Console.CmdArgs.Text
+import Data.List
 
 instance Show (Mode a) where
     show = show . helpText
 
 
 helpText :: Mode a -> [Text]
-helpText _ = [Line "Help output"]
+helpText m = helpGroup helpMode (modeGroupList m) ++
+            [Line "" | null $ modeList m] ++
+            helpGroup helpFlag (modeGroupFlags m)
+
+
+helpGroup :: (a -> [Text]) -> Group a -> [Text]
+helpGroup f xs = concat [[Line x | x /= ""] ++ concatMap f y | (x,y) <- xs]
+
+
+helpMode :: ([Name],Mode a) -> [Text]
+helpMode (xs,y) = [Cols [commas xs, modeHelp y]]
+
+
+helpFlag :: Flag a -> [Text]
+helpFlag x = case flagInfo x of
+    FlagUnnamed -> []
+    FlagNamed typ names -> [Cols [commas $ map ("-"++) a2, commas $ map ("--"++) b2, flagHelp x]]
+        where
+            (a,b) = partition ((==) 1 . length) names
+            (a2,b2) = if null b then (add a opt, b) else (a, add b opt)
+            add x y = if null x then x else (head x ++ y) : tail x
+            hlp = if null (flagType x) then "ITEM" else flagType x
+            opt = case typ of
+                ArgReq -> '=' : hlp
+                ArgOpt x -> "[=" ++ hlp ++ "]"
+                _ -> ""
+
+
+commas = intercalate ", "
