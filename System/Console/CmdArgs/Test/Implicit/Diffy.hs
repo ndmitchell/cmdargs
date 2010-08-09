7e88d074459bc2d49ade9ddae733ea9e88821ae8
Neil Mitchell <ndmitchell@gmail.com>
2010-08-09 06:24:40 +0000

    #231, add some tests for Maybe fields

--- System/Console/CmdArgs/Test/Implicit/Diffy.hs
+++ System/Console/CmdArgs/Test/Implicit/Diffy.hs
@@ -3,14 +3,14 @@ module System.Console.CmdArgs.Test.Implicit.Diffy where
 import System.Console.CmdArgs
 import System.Console.CmdArgs.Test.Implicit.Util
 
-data Diffy = Create {src :: FilePath, out :: FilePath}
+data Diffy = Create {src :: Maybe FilePath, out :: FilePath}
            | Diff {old :: FilePath, new :: FilePath, out :: FilePath}
              deriving (Data,Typeable,Show,Eq)
 
 outFlags x = x &= help "Output file" &= typFile
 
 create = Create
-    {src = "." &= help "Source directory" &= typDir
+    {src = def &= help "Source directory" &= typDir
     ,out = outFlags "ls.txt"
     } &= help "Create a fingerprint"
 
@@ -34,8 +34,8 @@ test = do
     fails ["create","--quiet"]
     fails ["create","--verbose"]
     isVerbosity ["create"] Normal
-    ["create","--src","x"] === create{src="x"}
-    ["create","--src","x","--src","y"] === create{src="y"}
+    ["create","--src","x"] === create{src=Just "x"}
+    ["create","--src","x","--src","y"] === create{src=Just "y"}
     fails ["diff","--src","x"]
     fails ["create","foo"]
     ["diff","foo1","foo2"] === diff{old="foo1",new="foo2"}
