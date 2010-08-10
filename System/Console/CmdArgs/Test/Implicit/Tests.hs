e14a3f8fdaafe5795d3901a7bf0547cebc2116d6
Neil Mitchell <ndmitchell@gmail.com>
2010-08-10 06:56:27 +0000

    Simplify the tests, there will probably end up being multiple tests in this file

--- System/Console/CmdArgs/Test/Implicit/Tests.hs
+++ System/Console/CmdArgs/Test/Implicit/Tests.hs
@@ -4,34 +4,31 @@ import System.Console.CmdArgs
 import System.Console.CmdArgs.Test.Implicit.Util
 
 data Test1
-    = Reading {maybeInt :: Maybe Int, listDouble :: [Double], maybeStr :: Maybe String, float :: Float
-              ,bool :: Bool, maybeBool :: Maybe Bool, listBool :: [Bool]}
-    | Other
+    = Test1 {maybeInt :: Maybe Int, listDouble :: [Double], maybeStr :: Maybe String, float :: Float
+            ,bool :: Bool, maybeBool :: Maybe Bool, listBool :: [Bool]}
       deriving (Show,Eq,Data,Typeable)
 
-reading = Reading def def def (def &= args) def def def
-
-mode1 = cmdArgsMode $ modes [reading,Other]  &= help "Testing various corner cases (1)"
+def1 = Test1 def def def (def &= args) def def def
+mode1 = cmdArgsMode $ def1 &= help "Testing various corner cases (1)"
 
 test1 = do
     let Tester{fails,(===)} = tester "Test1" mode1
-    fails []
-    ["reading"] === reading
-    ["reading","--maybeint=12"] === reading{maybeInt = Just 12}
-    ["reading","--maybeint=12","--maybeint=14"] === reading{maybeInt = Just 14}
-    fails ["reading","--maybeint"]
-    fails ["reading","--maybeint=test"]
-    ["reading","--listdouble=1","--listdouble=3","--listdouble=2"] === reading{listDouble=[1,3,2]}
-    fails ["reading","--maybestr"]
-    ["reading","--maybestr="] === reading{maybeStr=Just ""}
-    ["reading","--maybestr=test"] === reading{maybeStr=Just "test"}
-    ["reading","12.5"] === reading{float=12.5}
-    ["reading","12.5","18"] === reading{float=18}
-    ["reading","--bool"] === reading{bool=True}
-    ["reading","--maybebool"] === reading{maybeBool=Just True}
-    ["reading","--maybebool=off"] === reading{maybeBool=Just False}
-    ["reading","--listbool","--listbool=true","--listbool=false"] === reading{listBool=[True,True,False]}
-    fails ["reading","--listbool=fred"]
+    [] === def1
+    ["--maybeint=12"] === def1{maybeInt = Just 12}
+    ["--maybeint=12","--maybeint=14"] === def1{maybeInt = Just 14}
+    fails ["--maybeint"]
+    fails ["--maybeint=test"]
+    ["--listdouble=1","--listdouble=3","--listdouble=2"] === def1{listDouble=[1,3,2]}
+    fails ["--maybestr"]
+    ["--maybestr="] === def1{maybeStr=Just ""}
+    ["--maybestr=test"] === def1{maybeStr=Just "test"}
+    ["12.5"] === def1{float=12.5}
+    ["12.5","18"] === def1{float=18}
+    ["--bool"] === def1{bool=True}
+    ["--maybebool"] === def1{maybeBool=Just True}
+    ["--maybebool=off"] === def1{maybeBool=Just False}
+    ["--listbool","--listbool=true","--listbool=false"] === def1{listBool=[True,True,False]}
+    fails ["--listbool=fred"]
 
 
 -- from bug #230
