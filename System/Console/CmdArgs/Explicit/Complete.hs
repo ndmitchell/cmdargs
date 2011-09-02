c72048eaaca7d9525f8875965dd588e6d1af8b4d
Neil Mitchell <ndmitchell@gmail.com>
2011-09-02 06:49:11 +0000

    Significant work on getting the completions working better

--- System/Console/CmdArgs/Explicit/Complete.hs
+++ System/Console/CmdArgs/Explicit/Complete.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE PatternGuards #-}
 
 -- | This module does command line completion
 module System.Console.CmdArgs.Explicit.Complete(Complete(..), complete) where
 
 import System.Console.CmdArgs.Explicit.Type
+import Control.Monad
+import Data.List
+import Data.Maybe
 
 
 -- | How to complete a command line option.
@@ -12,10 +16,106 @@ data Complete
     | CompleteValue String -- ^ Complete to a particular value
       deriving (Eq,Ord,Show)
 
+
+prepend :: String -> Complete -> Complete
+prepend a (CompleteFile b c) = CompleteFile (a++b) c
+prepend a (CompleteDir b c) = CompleteDir (a++b) c
+prepend a (CompleteValue b) = CompleteValue (a++b)
+
+
 -- | Given a mode, a set of arguments that have already been entered, and the argument you are currently editing,
 --   return a set of the commands you could type now, in preference order.
 complete :: Mode a -> [String] -> Int -> [Complete]
-complete mode args i = [CompleteValue next]
+-- Roll forward looking at modes, and if you match a mode, enter it
+-- If the person just before is a flag without arg, look at how you can complete that arg
+-- If your prefix is a complete flag look how you can complete that flag
+-- If your prefix looks like a flag, look for legitimate flags
+-- Otherwise give a file/dir if they are arguments to this mode, and all flags
+-- If you haven't seen any args/flags then also autocomplete to any child modes
+complete mode_ args_ i = nub $ followArgs mode args now
     where
-        (seen,nxt) = splitAt i args
-        next = head $ nxt ++ [""]
+        (seen,next) = splitAt i args_
+        now = head $ next ++ [""]
+        (mode,args) = followModes mode_ seen
+
+
+-- | Given a mode and some arguments, try and drill down into the mode
+followModes :: Mode a -> [String] -> (Mode a, [String])
+followModes m (x:xs) | Just m2 <- pickBy modeNames x $ modeModes m = followModes m2 xs
+followModes m xs = (m,xs)
+
+
+pickBy :: (a -> [String]) -> String -> [a] -> Maybe a
+pickBy f name xs = find (\x -> name `elem` f x) xs `mplus` 
+                   find (\x -> any (name `isPrefixOf`) (f x)) xs
+
+
+-- | Follow args deals with all seen arguments, then calls on to deal with the next one
+followArgs :: Mode a -> [String] -> (String -> [Complete])
+followArgs m = first
+    where
+        first [] = expectArgFlagMode (modeModes m) (argsPick 0) (modeFlags m)
+        first xs = norm 0 xs
+
+        -- i is the number of arguments that have gone past
+        norm i [] = expectArgFlag (argsPick i) (modeFlags m)
+        norm i ("--":xs) = expectArg $ argsPick (i + length xs)
+        norm i (('-':'-':x):xs) | null b, flagInfo flg == FlagReq = val i flg xs
+                                | otherwise = norm i xs
+            where (a,b) = break (== '=') x
+                  flg = getFlag a
+        norm i (('-':x:y):xs) = case flagInfo flg of
+            FlagReq | null y -> val i flg xs
+                    | otherwise -> norm i xs
+            FlagOpt{} -> norm i xs
+            _ | "=" `isPrefixOf` y -> norm i xs
+              | null y -> norm i xs
+              | otherwise -> norm i (('-':y):xs)
+            where flg = getFlag [x]
+        norm i (x:xs) = norm (i+1) xs
+
+        val i flg [] = expectVal flg
+        val i flg (x:xs) = norm i xs
+
+        argsPick i = let (lst,end) = modeArgs m in if i < length lst then Just $ lst !! i else end
+
+        -- if you can't find the flag, pick one that is FlagNone (has all the right fallback)
+        getFlag x = fromMaybe (flagNone [] id "") $ pickBy flagNames x $ modeFlags m
+
+
+expectArgFlagMode :: [Mode a] -> Maybe (Arg a) -> [Flag a] -> String -> [Complete]
+expectArgFlagMode mode arg flag x
+    | "-" `isPrefixOf` x = expectFlag flag x
+    | otherwise = expectMode mode x ++ expectArg arg x ++ expectFlag flag x
+
+expectArgFlag :: Maybe (Arg a) -> [Flag a] -> String -> [Complete]
+expectArgFlag arg flag x
+    | "-" `isPrefixOf` x = expectFlag flag x
+    | otherwise = expectArg arg x ++ expectFlag flag x
+
+expectMode :: [Mode a] -> String -> [Complete]
+expectMode mode = expectStrings (map modeNames mode)
+
+expectArg :: Maybe (Arg a) -> String -> [Complete]
+expectArg Nothing x = []
+expectArg (Just arg) x = expectFlagHelp (argType arg) x
+
+expectFlag :: [Flag a] -> String -> [Complete]
+expectFlag flag x
+    | (a,_:b) <- break (== '=') x = case pickBy (map f . flagNames) a flag of
+        Nothing -> []
+        Just flg -> map (prepend (a ++ "=")) $ expectVal flg b
+    | otherwise = expectStrings (map (map f . flagNames) flag) x
+    where f x = "-" ++ ['-' | length x > 1] ++ x
+
+expectVal :: Flag a -> String -> [Complete]
+expectVal flg = expectFlagHelp (flagType flg)
+
+expectStrings :: [[String]] -> String -> [Complete]
+expectStrings xs x = map CompleteValue $ concatMap (take 1 . filter (x `isPrefixOf`)) xs
+
+expectFlagHelp :: FlagHelp -> String -> [Complete]
+expectFlagHelp typ x = case typ of
+    "FILE" -> [CompleteFile "" x]
+    "DIR" -> [CompleteDir "" x]
+    _ -> []
