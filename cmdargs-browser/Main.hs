658a6382a18adde54a8dd3d2e3d5cb6951c96fe6
Neil Mitchell <ndmitchell@gmail.com>
2012-10-27 22:32:53 +0000

    Add the JSON formatted output into the help page

--- cmdargs-browser/Main.hs
+++ cmdargs-browser/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances, OverlappingInstances #-}
 
 module Main where
 
@@ -15,6 +17,7 @@ import Control.Arrow
 import System.Environment
 import System.FilePath
 import Data.Maybe
+import Data.List
 import System.Console.CmdArgs.Helper
 import System.Console.CmdArgs.Explicit
 import Paths_cmdargs_browser
@@ -79,6 +82,9 @@ contents mode = unlines
     ,"<title>" ++ prog ++ " arguments</title>"
     ,"<link type='image/png' rel='icon' href='/res/cmdargs.png' />"
     ,"<script src='/res/jquery-1.4.2.js'></script>"
+    ,"<script>"
+    ,"var mode = " ++ json mode ++ ";"
+    ,"</script>"
     ,"<script src='/res/cmdargs.js'></script>"
     ,"<link type='text/css' rel='stylesheet' href='res/cmdargs.css' />"
     ,"</head>"
@@ -89,3 +95,67 @@ contents mode = unlines
     ]
     where
         prog = head $ modeNames mode ++ ["program"]
+
+
+---------------------------------------------------------------------
+-- JSON conversion
+
+class JSON a where json :: a -> String
+
+(@=) :: JSON a => String -> a -> (String,String)
+(@=) a b = (a, json b)
+
+jsonFields :: [(String, String)] -> String
+jsonFields xs = "{" ++ intercalate "," [a ++ ":" ++ b | (a,b) <- xs] ++ "}"
+
+instance JSON Bool where json = show
+instance JSON [Char] where json = show
+
+instance JSON a => JSON [a] where
+    json xs = "[" ++ intercalate "," (map json xs) ++ "]"
+
+instance JSON a => JSON (Maybe a) where
+    json Nothing = "null"
+    json (Just x) = json x
+
+instance (JSON a, JSON b) => JSON (a, b) where
+    json (a, b) = "[" ++ json a ++ "," ++ json b ++ "]"
+
+
+instance JSON (Mode a) where
+    json Mode{..} = json
+        ["modes" @= modeGroupModes
+        ,"names" @= modeNames
+        ,"expandAt" @= modeExpandAt
+        ,"help" @= modeHelp
+        ,"helpSuffix" @= modeHelpSuffix
+        ,"args" @= modeArgs
+        ,"flags" @= modeGroupFlags
+        ]
+
+instance JSON a => JSON (Group a) where
+    json Group{..} = jsonFields
+        ["unnamed" @= groupUnnamed
+        ,"hidden" @= groupHidden
+        ,"named" @= groupNamed
+        ]
+
+instance JSON (Flag a) where
+    json Flag{..} = jsonFields
+        ["names" @= flagNames
+        ,"info" @= flagInfo
+        ,"type" @= flagType
+        ,"help" @= flagHelp
+        ]
+
+instance JSON FlagInfo where
+    json FlagReq = jsonFields ["type" @= "req"]
+    json (FlagOpt x) = jsonFields ["type" @= "opt", "value" @= x]
+    json (FlagOptRare x) = jsonFields ["type" @= "rare", "value" @= x]
+    json FlagNone = jsonFields ["type" @= "none"]
+
+instance JSON (Arg a) where
+    json Arg{..} = jsonFields
+        ["type" @= argType
+        ,"require" @= argRequire
+        ]
